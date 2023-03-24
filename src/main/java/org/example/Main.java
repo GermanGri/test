73322e0805fe44dbf74ce7b94127f62853f0f645
Razin14 <german.gribanov@gmail.com>
2023-03-24 18:18:59 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.setProperty("web-driver.chrome.driver","/Users/german/chromedriver_mac_arm64 (1)");
+        WebDriver driver = new ChromeDriver();
+//        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+
+
+
+    }
+}
